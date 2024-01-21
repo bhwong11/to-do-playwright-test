40c8f5e79154bd12c13b3350db435bc3a06f2335
Bryant Wong <bryanthwong@gmail.com>
2024-01-20 23:33:14 -0500

    added show and hide dit

--- src/app/(components)/ToDo.js
+++ src/app/(components)/ToDo.js
@@ -1,15 +1,20 @@
 "use client"
 import EditToDo from "./EditToDo"
 import { useSelector } from "react-redux"
+import { useState } from "react"
 
 const ToDo = ({id})=>{
   const {title, description} = useSelector(state=>state.toDos.find(toDo=>toDo.id===id))
+  const [showEdit,setShowEdit]= useState(false)
 
   return (
     <div className="border-2 border-grey-500 my-2 p-2">
       <div>{title}</div>
       <p>{description}</p>
-      <EditToDo id={id} title={title} description={description}/>
+      <button className="link" onClick={()=>setShowEdit(prev=>!prev)}>
+        {showEdit?"hide edit":"show edit"}
+      </button>
+      {showEdit && <EditToDo id={id} title={title} description={description}/>}
     </div>
   )
 }
