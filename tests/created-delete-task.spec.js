f5a3e2c8f7012993520ef2beaf184e0aeb569e21
Bryant Wong <bryanthwong@gmail.com>
2024-01-24 15:26:45 -0500

    working remove test

--- tests/created-delete-task.spec.js
+++ tests/created-delete-task.spec.js
@@ -10,47 +10,121 @@ const testTasks = [{
   },{
     title:"test task title 3",
     description: "test task description 3"
-  }]
+  }
+]
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:3002/to-do-list');
-});
+const editedTask = {
+  title:"test new title",
+  description: "test new description"
+}
 
-test('page has correct title', async ({ page }) => {
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/To Dos/);
-});
+test.describe('Create delete functionality', () => {
 
-test('created task displays', async ({ page }) => {
-  const titleInput = page.getByRole('textbox', { name: 'title' })
-  const descriptionInput = page.getByRole('textbox', { name: 'description' })
-  const createButton = page.getByRole('button', { name: 'finish Add ToDo' })
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3002/to-do-list');
+  });
 
-  await titleInput.fill(testTasks[0].title)
-  await descriptionInput.fill(testTasks[0].description)
-  await createButton.click()
-
-  const newToDo = page.getByText(testTasks[0].title)
+  test.afterEach( async({ page })=>{
+    console.log('done')
+    // const page = await browser.newPage()
+    await page.goto('http://localhost:3002/to-do-list');
+    const newToDoTitle = page.getByRole('paragraph').filter({hasText:testTasks[0].title})
+    const newToDo = page.locator('.to-do', {has: newToDoTitle}).first()
+    const newToDoShowEditButton = newToDo.getByRole('button', { name: 'show edit' }).first()
   
-  await expect(newToDo).toBeVisible();
-});
+    const isNewToDoShowEditButtonVisible = await newToDoShowEditButton.isVisible()
+    console.log('isNewToDoShowEditButtonVisible',isNewToDoShowEditButtonVisible)
+    // await newToDoShowEditButton.click()
+    // await page.getByRole('button', { name: 'delete ToDo' }).click()
+    if(isNewToDoShowEditButtonVisible){
+      await newToDoShowEditButton.click()
+      await page.getByRole('button', { name: 'delete ToDo' }).click()
+    }
+  
+    const editedToDoTitle = page.getByRole('paragraph').filter({hasText: 'test'})
+    const editedToDo = page.locator('.to-do', {has: editedToDoTitle}).first()
+    const editedToDoShowEditButton = editedToDo.getByRole('button', { name: 'show edit' }).first()
+    
+    const isEditedToDoShowEditButtonVisible = await editedToDoTitle.isVisible()
+    console.log('is visible', isEditedToDoShowEditButtonVisible)
+  
+    await editedToDoShowEditButton.click()
+    await page.getByRole('button', { name: 'delete ToDo' }).click()
+  
+    if(isEditedToDoShowEditButtonVisible){
+      await editedToDoShowEditButton.click()
+      await page.getByRole('button', { name: 'delete ToDo' }).click()
+    }
+  
+  })
 
-test('deleted task displays', async ({ page }) => {
-  // Expect a title "to contain" a substring.
-  const titleInput = page.getByRole('textbox', { name: 'title' })
-  const descriptionInput = page.getByRole('textbox', { name: 'description' })
-  const createButton = page.getByRole('button', { name: 'finish Add ToDo' })
+  test('page has correct title', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/To Dos/);
+  });
 
-  await titleInput.fill(testTasks[1].title)
-  await descriptionInput.fill(testTasks[1].description)
-  await createButton.click()
+  test('created task displays', async ({ page }) => {
 
-  const newToDoTitle = page.getByRole('paragraph').filter({hasText:testTasks[1].title})
-  const newToDo = page.locator('.to-do', {has: newToDoTitle}).first()
-  const newToDoShowEditButton = newToDo.getByRole('button', { name: 'show edit' }).first()
-  await newToDoShowEditButton.click()
+    const titleInput = page.getByRole('textbox', { name: 'title' })
+    const descriptionInput = page.getByRole('textbox', { name: 'description' })
+    const createButton = page.getByRole('button', { name: 'finish Add ToDo' })
 
-  await page.getByRole('button', { name: 'delete ToDo' }).click()
+    await titleInput.fill(testTasks[0].title)
+    await descriptionInput.fill(testTasks[0].description)
+    await createButton.click()
 
-  await expect(newToDo).toBeVisible();
-});
+    const newToDo = page.getByText(testTasks[0].title).first()
+    
+    await expect(newToDo).toBeVisible();
+  });
+
+  test('deleted task disappears', async ({ page }) => {
+
+    const titleInput = page.getByRole('textbox', { name: 'title' })
+    const descriptionInput = page.getByRole('textbox', { name: 'description' })
+    const createButton = page.getByRole('button', { name: 'finish Add ToDo' })
+
+    await titleInput.fill(testTasks[0].title)
+    await descriptionInput.fill(testTasks[0].description)
+    await createButton.click()
+
+    const newToDoTitle = page.getByRole('paragraph').filter({hasText:testTasks[0].title})
+    const newToDo = page.locator('.to-do', {has: newToDoTitle}).first()
+    const newToDoShowEditButton = newToDo.getByRole('button', { name: 'show edit' }).first()
+    await newToDoShowEditButton.click()
+
+    await page.getByRole('button', { name: 'delete ToDo' }).click()
+
+    await expect(newToDo).toBeHidden();
+  })
+
+  test('edited task appears', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    const titleInput = page.getByRole('textbox', { name: 'title' })
+    const descriptionInput = page.getByRole('textbox', { name: 'description' })
+    const createButton = page.getByRole('button', { name: 'finish Add ToDo' })
+
+    await titleInput.fill(testTasks[1].title)
+    await descriptionInput.fill(testTasks[1].description)
+    await createButton.click()
+
+    const newToDoTitle = page.getByRole('paragraph').filter({hasText:testTasks[1].title})
+    const newToDo = page.locator('.to-do', {has: newToDoTitle}).first()
+    const newToDoShowEditButton = newToDo.getByRole('button', { name: 'show edit' }).first()
+    await newToDoShowEditButton.click()
+
+    await page.getByRole('button', { name: 'edit ToDo' }).click()
+
+    await titleInput.first().fill(editedTask.title)
+    await descriptionInput.first().fill(editedTask.description)
+    const editButton = page.getByRole('button', { name: 'finish Edit ToDo' })
+    await editButton.click()
+
+    const editedToDoTitle = page.getByRole('paragraph').filter({hasText:editedTask.title})
+    const editedToDoDescription = page.getByRole('paragraph').filter({hasText:editedTask.description})
+    const editedToDo = page.locator('.to-do', {has: editedToDoTitle}).and(page.locator('.to-do', {has: editedToDoDescription})).first()
+
+    await expect(editedToDo).toBeVisible();
+  })
+
+})
